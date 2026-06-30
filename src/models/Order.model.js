0fc57f9c0db9f1e4045d1eda3da84c98322ea299
Malik Ubaid <Malik786-ubaid>
2026-06-30 01:26:18 -0700

    change a liitle bit

--- src/models/Order.model.js
+++ src/models/Order.model.js
@@ -14,11 +14,31 @@ const orderSchema = new mongoose.Schema(
       required: true,
     },
 
-   freelancer: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: "User",
-  required: true,
-},
+    freelancer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    projectTitle: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    budget: {
+      type: Number,
+      required: true,
+    },
+
+    deadline: {
+      type: Date,
+      required: true,
+    },
 
     price: {
       type: Number,
